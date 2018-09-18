1ccb872fd141d997d29859ceaa71f541ac7daaad
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-09-18 15:47:05 -0700

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>MarioPlan</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>MarioPlan</h1>
+        </div>
+      </Router>
     );
   }
 }
