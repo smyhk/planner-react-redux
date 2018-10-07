8fa160612249b75a56531a1cf2b09daa8d3bc412
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-10-07 09:05:26 -0700

    Connect Redux to Firebase

--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 
 import rootReducer from './store/reducers/rootReducer';
+import fbConfig from './config/fbConfig';
 
-// Redux store takes a root reducer; uses middleware to make async calls to db
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// Redux store takes a 'root reducer'; uses middleware to make async calls to db
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore
+      })
+    ),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // Make async call to database
 
     dispatch({ type: 'CREATE_PROJECT', project: project });
