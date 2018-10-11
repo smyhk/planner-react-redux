44f5732fa15dc5e6b870ba8021c7e75bfeccce68
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-10-11 10:35:02 -0700

    Implement signin with Firebase auth

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,8 +1,25 @@
 // Default state
-const initState = {};
+const initState = {
+  authError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case 'LOGIN_ERROR':
+      console.info('login error');
+      return {
+        ...state,
+        authError: 'Login failed'
+      };
+    case 'LOGIN_SUCCESS':
+      console.info('login success');
+      return {
+        ...state,
+        authError: null
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
